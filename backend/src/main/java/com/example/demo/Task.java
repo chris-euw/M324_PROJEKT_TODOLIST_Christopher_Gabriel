5adf2185f09004f6ea3e8ff9cb9c4f8ea12edb8f
Christopher Söll <chris3270015@gmail.com>
2026-03-27 17:47:12 +0100

    add: Implemented the Code for the features of the Test for TDD, Deleted some dependencies causing Tests not to work

--- backend/src/main/java/com/example/demo/Task.java
+++ backend/src/main/java/com/example/demo/Task.java
@@ -10,11 +10,13 @@ public class Task {
     
     private String taskdescription;
     private String creationDate;
+    private boolean completed;
 
     //Zeitstempel wird erstellt
     public Task() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         this.creationDate = LocalDateTime.now().format(formatter);
+        this.completed = false;
     }
 
     //Getter und Setter
@@ -33,5 +35,11 @@ public class Task {
     //Überschreiben des Erstellungsdatums ist nicht vorgesehen, aber hier ist der Setter, falls es doch notwendig sein sollte
     public void setCreationDate(String creationDate) {
         this.creationDate = creationDate;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
